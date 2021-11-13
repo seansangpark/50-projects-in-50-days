7d095449f9646a54709f8ad199dc27a05b9ad0f7
Sean Park <seansangpark@gmail.com>
2021-11-13 12:02:21 -0800

    Refactored using async await. Dad Jokes complete

--- 10_DadJokes/script.js
+++ 10_DadJokes/script.js
@@ -1,17 +1,31 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-const generateJoke = () => {
+// const generateJoke = () => {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => (jokeEl.innerHTML = data.joke));
+// };
+
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   };
 
-  fetch('https://icanhazdadjoke.com/', config)
-    .then((res) => res.json())
-    .then((data) => (jokeEl.innerHTML = data.joke));
-};
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
 
 generateJoke();
 
