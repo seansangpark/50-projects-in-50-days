f551b486f7198daa8f7c9c80a37001229b081fa0
Sean Park <seansangpark@gmail.com>
2021-11-22 12:50:18 -0800

    Small cup fill functionality applied

---
+++ 16_DrinkWater/script.js
@@ -0,0 +1,25 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => highlightCups(idx));
+});
+
+const highlightCups = (idx) => {
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, index) => {
+    if (index <= idx) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+};
