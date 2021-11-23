47945677993c0e4309fcd9791a323898d3d46ca0
Sean Park <seansangpark@gmail.com>
2021-11-23 13:24:49 -0800

    Dynamically fetching data from api using default and search input

--- 17_MovieApp/script.js
+++ 17_MovieApp/script.js
@@ -6,6 +6,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=44f407669b7845cae76ffcb1f548f3ce&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -16,7 +17,7 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
 }
 
 form.addEventListener('submit', (e) => {
@@ -32,3 +33,44 @@ form.addEventListener('submit', (e) => {
     window.location.reload();
   }
 });
+
+function showMovies(movies) {
+  main.innerHTML = '';
+  console.log(movies);
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+      <div class="movie">
+        <img
+          src="${IMG_PATH + poster_path}"
+          alt="${title}"
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+      </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
