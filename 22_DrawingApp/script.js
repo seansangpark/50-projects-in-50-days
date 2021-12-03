16cfb0a85db8494fd32759b1d8cc465bd3b34af4
Sean Park <seansangpark@gmail.com>
2021-12-03 10:11:54 -0800

    Mouse events and drawing functionality implemented

--- 22_DrawingApp/script.js
+++ 22_DrawingApp/script.js
@@ -2,9 +2,37 @@ const cavas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x;
 let y;
+
+canvas.addEventListener('mousedown', (event) => {
+  isPressed = true;
+
+  x = event.offsetX;
+  y = event.offsetY;
+});
+
+canvas.addEventListener('mouseup', (event) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', (event) => {
+  if (isPressed) {
+    const x2 = event.offsetX;
+    const y2 = event.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -18,9 +46,9 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
 
-drawCircle(100, 200);
-drawLine(300, 300, 400, 500);
+// drawCircle(100, 200);
+// drawLine(300, 300, 400, 500);
