70cdf3b94b1ac1779452402409a5b9f3cb5d382d
Sean Park <seansangpark@gmail.com>
2021-12-17 10:56:14 -0800

    Card displaying dynamically after successful get request

--- 28_GithubProfiles/script.js
+++ 28_GithubProfiles/script.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.github.com/users/';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -7,9 +8,49 @@ async function getUser(username) {
   try {
     const { data } = await axios.get(APIURL + username);
     console.log(data);
+    createUserCard(data);
   } catch (error) {
-    console.log(error);
+    if (error.response.status == 404) {
+      createErrorCard('No profile with this username');
+    }
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+      <div class="card">
+      <div>
+        <img
+          src="${user.avatar_url}"
+          alt=""
+          class="avatar"
+        />
+      </div>
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class='card'>
+      <h1>${msg}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
