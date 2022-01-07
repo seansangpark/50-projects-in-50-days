dce08aa4b17968b1c95fc7f84fb1b33724a43db5
Sean Park <seansangpark@gmail.com>
2022-01-07 10:06:41 -0800

    Auto text effect functionality implemented

---
+++ 30_AutoTextEffect/script.js
@@ -0,0 +1,22 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'I am a Software Engineer for Checkr';
+
+let idx = 1;
+let speed = 100 / speedEl.value;
+
+const writeText = () => {
+  textEl.innerText = text.slice(0, idx);
+
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+};
+
+speedEl.addEventListener('input', (e) => (speed = 300 / e.target.value));
+
+writeText();
