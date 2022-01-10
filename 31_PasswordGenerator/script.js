28cd792a759f0253001d723bbead6001a0e3fe09
Sean Park <seansangpark@gmail.com>
2022-01-10 08:13:00 -0800

    Helper functions created

---
+++ 31_PasswordGenerator/script.js
@@ -0,0 +1,26 @@
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+// Helper Functions
+const getRandomLower = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getRandomUpper = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getRandomNumber = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+const getRandomSymbol = () => {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getRandomSymbol());
