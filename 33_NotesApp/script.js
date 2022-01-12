c53fdfbc09760b63a35fd3b676e64b8f9f6c0639
Sean Park <seansangpark@gmail.com>
2022-01-12 08:54:39 -0800

    Implemented local storage to save and delete notes. Project complete

--- 33_NotesApp/script.js
+++ 33_NotesApp/script.js
@@ -1,17 +1,18 @@
 const addBtn = document.getElementById('add');
+const notes = JSON.parse(localStorage.getItem('notes'));
 
 const addNewNote = (text = '') => {
   const note = document.createElement('div');
   note.classList.add('note');
 
   note.innerHTML = `
-  <div class="tools">
-    <button class="edit"><i class="fas fa-edit"></i></button>
-    <button class="delete"><i class="fas fa-trash-alt"></i></button>
-  </div>
+    <div class="tools">
+      <button class="edit"><i class="fas fa-edit"></i></button>
+      <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
 
-  <div class="main ${text ? '' : 'hidden'}"></div>
-  <textarea class="${text ? 'hidden' : ''}"></textarea>
+    <div class="main ${text ? '' : 'hidden'}"></div>
+    <textarea class="${text ? 'hidden' : ''}"></textarea>
   `;
 
   const editBtn = note.querySelector('.edit');
@@ -22,7 +23,11 @@ const addNewNote = (text = '') => {
   textArea.value = text;
   main.innerHTML = marked.parse(text);
 
-  deleteBtn.addEventListener('click', () => note.remove());
+  deleteBtn.addEventListener('click', () => {
+    note.remove();
+
+    updateLS();
+  });
 
   editBtn.addEventListener('click', () => {
     main.classList.toggle('hidden');
@@ -33,9 +38,25 @@ const addNewNote = (text = '') => {
     const { value } = e.target;
 
     main.innerHTML = marked.parse(value);
+
+    updateLS();
   });
 
   document.body.appendChild(note);
 };
 
+const updateLS = () => {
+  const notesText = document.querySelectorAll('textarea');
+
+  const notes = [];
+
+  notesText.forEach((note) => notes.push(note.value));
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+};
+
 addBtn.addEventListener('click', () => addNewNote());
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
